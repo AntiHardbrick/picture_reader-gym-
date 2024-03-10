c65d7179eb03455fe977075b3ff9998f68ba8750
AntiHardbrick <dndtks1626@gmail.com>
2024-03-11 03:18:48 +0900

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace picture_reader_gym_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            picturebox_main = new PictureBox();
+            textedit_path = new TextBox();
+            button_search = new Button();
+            button_execute = new Button();
+            statusStrip1 = new StatusStrip();
+            footer_status = new ToolStripStatusLabel();
+            button_reset = new Button();
+            ((System.ComponentModel.ISupportInitialize)picturebox_main).BeginInit();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // picturebox_main
+            // 
+            picturebox_main.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            picturebox_main.BackColor = Color.Silver;
+            picturebox_main.Location = new Point(12, 12);
+            picturebox_main.Name = "picturebox_main";
+            picturebox_main.Size = new Size(472, 308);
+            picturebox_main.SizeMode = PictureBoxSizeMode.StretchImage;
+            picturebox_main.TabIndex = 0;
+            picturebox_main.TabStop = false;
+            // 
+            // textedit_path
+            // 
+            textedit_path.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textedit_path.Location = new Point(104, 326);
+            textedit_path.Name = "textedit_path";
+            textedit_path.Size = new Size(332, 23);
+            textedit_path.TabIndex = 1;
+            // 
+            // button_search
+            // 
+            button_search.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button_search.Location = new Point(56, 326);
+            button_search.Name = "button_search";
+            button_search.Size = new Size(42, 23);
+            button_search.TabIndex = 2;
+            button_search.Text = "path";
+            button_search.UseVisualStyleBackColor = true;
+            button_search.Click += button_search_Click;
+            // 
+            // button_execute
+            // 
+            button_execute.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button_execute.Location = new Point(12, 326);
+            button_execute.Name = "button_execute";
+            button_execute.Size = new Size(38, 23);
+            button_execute.TabIndex = 3;
+            button_execute.Text = "▶";
+            button_execute.UseVisualStyleBackColor = true;
+            button_execute.Click += button_execute_Click;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { footer_status });
+            statusStrip1.Location = new Point(0, 352);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(496, 22);
+            statusStrip1.TabIndex = 4;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // footer_status
+            // 
+            footer_status.BackColor = Color.Transparent;
+            footer_status.Name = "footer_status";
+            footer_status.Size = new Size(38, 17);
+            footer_status.Text = "status";
+            // 
+            // button_reset
+            // 
+            button_reset.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button_reset.Location = new Point(442, 325);
+            button_reset.Name = "button_reset";
+            button_reset.Size = new Size(42, 23);
+            button_reset.TabIndex = 5;
+            button_reset.Text = "reset";
+            button_reset.UseVisualStyleBackColor = true;
+            button_reset.Click += button_reset_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(496, 374);
+            Controls.Add(button_reset);
+            Controls.Add(statusStrip1);
+            Controls.Add(button_execute);
+            Controls.Add(button_search);
+            Controls.Add(textedit_path);
+            Controls.Add(picturebox_main);
+            Name = "Form1";
+            Text = "picture reader";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)picturebox_main).EndInit();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox picturebox_main;
+        private TextBox textedit_path;
+        private Button button_search;
+        private Button button_execute;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel footer_status;
+        private Button button1;
+        private Button button_reset;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+
+namespace picture_reader_gym_ {
+    public partial class Form1 : Form {
+
+        static Form1 instance = null;
+        bool started = false;
+        List<string> file_blacklist;
+        List<(Image, string)> usedResource_list;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            footer_status.Text = "hi!";
+            instance = this;
+            file_blacklist = new List<string>();
+            usedResource_list = new List<(Image, string)>();
+        }
+
+        private void button_execute_Click(object sender, EventArgs e) {
+            if (!started) {
+                if (!Path.Exists(textedit_path.Text)) {
+                    MessageBox.Show("path missing");
+                    footer_status.Text = "check your folder!";
+                    return;
+                }
+                footer_status.Text = "running..";
+                started = true;
+                Task.Run(() => main_start());
+            }
+            else {
+                started = false;
+            }
+        }
+        /// <summary>
+        /// this function should be called asyncronously
+        /// </summary>
+        private void main_start() {
+            //Image bak_image = null;
+
+            string used_folder_path = Path.Combine(textedit_path.Text, "used");
+            if (!Directory.Exists(used_folder_path)) {
+                Directory.CreateDirectory(used_folder_path);
+            }
+
+            while (started) {
+                string[] files = Directory.GetFiles(textedit_path.Text);
+                //foreach (string file in files)
+                for (int t = 0; t < files.Length; t++) {
+                    bool blacked = false;
+                    //foreach (string black in file_blacklist)
+                    for (int i = 0; i < file_blacklist.Count; i++) {
+                        if (files[t] == file_blacklist[i]) {
+                            blacked = true;
+                            break;
+                        }
+                    }
+                    if (blacked) {
+                        continue;
+                    }
+
+                    StringBuilder name_without_frac_builder = new StringBuilder();
+                    string filename_noex = Path.GetFileNameWithoutExtension(files[t]);
+                    int period_index = 99;
+                    for (int j = 0; j < filename_noex.Length; j++) {
+                        if (j == period_index + 4)
+                            break;
+                        if (filename_noex[j] != '.') {
+                            name_without_frac_builder.Append(filename_noex[j]);
+                        }
+                        else {
+                            period_index = j;
+                        }
+                    }
+                    //MessageBox.Show($"file:{name_without_frac_builder.ToString()} cur:{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}");
+
+                    long file_timestamp = long.Parse(name_without_frac_builder.ToString());
+                    if (file_timestamp + 10000 > DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()) {
+                        //Thread.Sleep(100);
+                        break;
+                    }
+
+                    StringBuilder extensionbuilder = new StringBuilder();
+                    for (int i = files[t].Length - 3; i < files[t].Length; i++) {
+                        extensionbuilder.Append(files[t][i]);
+                    }
+                    if (extensionbuilder.ToString() == "png") {
+                        string folder_name = string.Empty;
+                        try {
+                            //Image bak_image = picturebox_main.Image;
+                            folder_name = Path.GetDirectoryName(files[t]);
+                            string filename = Path.GetFileName(files[t]);
+                            folder_name = Path.Combine(folder_name, "used", filename);
+                            File.Move(files[t], folder_name);
+
+                            Image image = Image.FromFile(folder_name);
+                            //used_imagePath_list.Add(files[t]);
+                            usedResource_list.Add((image, folder_name));
+                            picturebox_main.Image = image;
+
+                            if (usedResource_list.Count > 5) {
+                                usedResource_list[0].Item1.Dispose();
+                                File.Delete(usedResource_list[0].Item2);
+                                usedResource_list.RemoveAt(0);
+                            }
+                            //bak_image = picturebox_main.Image;
+                            //bak_image = image;
+                            //bak_path = files[t];
+                        }
+                        catch (IOException e) {
+                            //file_blacklist.Add(files[t]);
+                        }
+                        catch (OutOfMemoryException e) {
+                            file_blacklist.Add(folder_name);
+                        }
+                    }
+                    else {
+                        Form1.instance.Invoke(() => {
+                            footer_status.Text = $"** not png file here, please remove it manually [{extensionbuilder.ToString()}]**";
+                        });
+                    }
+                }
+
+                string bak_status = footer_status.Text ?? "";
+                int used_files_count = Directory.GetFiles(used_folder_path).Length;
+                Form1.instance.Invoke(() => {
+                    if (!bak_status.Contains('/')) {
+                        footer_status.Text = $"{bak_status}/total_files:[{files.Length}]/used_files:[{used_files_count}]/blacklist_length:[{file_blacklist.Count}]";
+                    }
+                    else {
+                        string[] splitted = bak_status.Split('/');
+                        string updated_status = $"{splitted[0]}/total_files:[{files.Length}]/used_files:[{used_files_count}]/blacklist_length:[{file_blacklist.Count}]";
+                        footer_status.Text = updated_status;
+                    }
+                });
+            }
+            Form1.instance.Invoke(() => {
+                footer_status.Text = "stopped!";
+            });
+        }
+
+
+        private void button_search_Click(object sender, EventArgs e) {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK) {
+                textedit_path.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void button_reset_Click(object sender, EventArgs e) {
+            if (started) {
+                footer_status.Text = $"[running] can't reset while running!";
+                return;
+            }
+
+            int total_blacklist = file_blacklist.Count;
+            file_blacklist.Clear();
+            Image cur_image = picturebox_main.Image;
+            if (cur_image != null) {
+                picturebox_main.Image = null;
+                cur_image.Dispose();
+            }
+
+            for (int i = usedResource_list.Count - 1; i >= 0; i--) {
+                usedResource_list[i].Item1.Dispose();
+                File.Delete(usedResource_list[i].Item2);
+            }
+            footer_status.Text = "clear saved array..";
+
+            string used_path = Path.Combine(textedit_path.Text, "used");
+            if (Directory.Exists(used_path)) {
+                string[] files = Directory.GetFiles(used_path);
+                for (int i = 0; i < files.Length; i++) {
+                    File.Delete(files[i]);
+                }
+                footer_status.Text = "delete all used-folder's image";
+            }
+
+            string status_text = string.Empty;
+            if (started)
+                status_text = "running";
+            else
+                status_text = "not-running";
+            footer_status.Text = $"[{status_text}] file blacklist cleared!(total: {total_blacklist})";
+        }
+    }
+}
